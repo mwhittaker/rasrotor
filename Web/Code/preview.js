9fa422b3167c74e1a9f85048d611ee7a923b16eb
Michael Whittaker <mjw297@cornell.edu>
2013-03-21 19:34:14 -0400

    Redid the media section again and gave it a really cool polaroid preview. We cannot figure out how to center the images without skewing them.

--- Web/Code/preview.js
+++ Web/Code/preview.js
@@ -1,33 +1,29 @@
 function preview(obj) {
     var previewPic = document.getElementById("previewPic"); // The eventual preview picture
     var url = obj.getAttribute("href"); // the url of the picture to preview
-//    var dimPic = new Image(); // the dimension testing pic
+    var para = document.getElementById("demo");
 
-    // calculate dimensions
-//    dimPic.src = url;
-//    var delWidth = (520-dimPic.width)/2;
-//    var delHeight = (390-dimPic.height)/2;    
+   
+    // create dimension image
+//    var dim = new Image();
+ //   dim.src = url;
+//    dim.style.width = '390px';
+//    dim.style.height = 'auto';
 
-    // assign url to preview
+ //   var disp = (355-parseInt(dim.style.height))/2;
+  
+  
+//    para.innerHTML = "url of image going into is: " + url + "; width is: " + dim.style.width + "; height is: " + dim.style.height;
+     
     previewPic.src = url;
-
-    // set height and width according to dimension measurements
-//    if (delWidth < delHeight) {
-//        previewPic.style.width = "520px";
-//        previewPic.style.height = "auto";
-//        preview.style.marginTop = delHeight.toString();
-//    }
-//    else {
-//        previewPic.style.height = "390px";
-//        previewPic.style.width = "auto";
-//    }
-    
-    // make visible
+ //   previewPic.style.marginTop = disp;
     previewPic.style.visibility = "visible";
 }
 
 function deview() {
-    var previewPic = document.getElementById("previewPic");
-    previewPic.style.visibility = "hidden";
+    var preview = document.getElementById("previewPic");
+    
+    //preview.src = null;
     previewPic.src = "../Media/Pixel.gif";
+    //preview.style.visibility = "hidden";
 }
