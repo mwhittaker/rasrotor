d06ecda0033cd35b3e4dd27ee7efce1b1e9772d4
Michael Whittaker <mjw297@cornell.edu>
2013-03-21 20:00:15 -0400

    Figured out how to center image previews!

--- Web/Code/preview.js
+++ Web/Code/preview.js
@@ -1,29 +1,10 @@
 function preview(obj) {
-    var previewPic = document.getElementById("previewPic"); // The eventual preview picture
-    var url = obj.getAttribute("href"); // the url of the picture to preview
-    var para = document.getElementById("demo");
-
-   
-    // create dimension image
-//    var dim = new Image();
- //   dim.src = url;
-//    dim.style.width = '390px';
-//    dim.style.height = 'auto';
-
- //   var disp = (355-parseInt(dim.style.height))/2;
-  
-  
-//    para.innerHTML = "url of image going into is: " + url + "; width is: " + dim.style.width + "; height is: " + dim.style.height;
-     
-    previewPic.src = url;
- //   previewPic.style.marginTop = disp;
-    previewPic.style.visibility = "visible";
+    var pane = document.getElementById("preview"); // The eventual preview picture
+    var urll = obj.getAttribute("href"); // the url of the picture to preview
+    pane.style.backgroundImage = "url(" + urll + ")";
 }
 
 function deview() {
-    var preview = document.getElementById("previewPic");
-    
-    //preview.src = null;
-    previewPic.src = "../Media/Pixel.gif";
-    //preview.style.visibility = "hidden";
+    var pane = document.getElementById("preview");
+    pane.style.backgroundImage = "";
 }
